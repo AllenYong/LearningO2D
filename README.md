025666993d495ba5b6ad4fe87b43a33ab7fb1a2e
AllenYong <dyyong@mail.ustc.edu.cn>
2015-04-15 00:13:51 +0800

    first commit: based environment

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a temp project used for testing Overlap2D
---
+++ desktop/src/com/mygdx/gameo2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gameo2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gameo2d.MyGdxGameO2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGameO2D(), config);
+	}
+}
